e8bd134e1b38b674329172b0264bd901785d8bd4
jplulu <lujason899@gmail.com>
2020-11-14 19:31:26 -0500

    User model done

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
@@ -13,5 +15,23 @@ class User(db.Model):
     last_name = db.Column(db.String(30), nullable=False)
     dob = db.Column(db.Date)
     phone_number = db.Column(db.String(20))
-    created_at = db.Column(db.Datatime)
-    user_type = db.(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=func.current_timestamp())
+    user_type = db.Column(db.Integer)
+
+    def __init__(self, email, first_name, last_name, dob, phone_number, user_type):
+        self.email = email
+        self.first_name = first_name
+        self.last_name = last_name
+        self.dob = dob
+        self.phone_number = phone_number
+        self.user_type = user_type
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+
+
+
