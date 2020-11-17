bcae6aaac828d40e22dfdbd396386eac9dbad24d
jplulu <lujason899@gmail.com>
2020-11-16 19:41:23 -0500

    All initial models done

--- backend/models.py
+++ backend/models.py
@@ -1,8 +1,25 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import func
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Flask
+import enum
 
-db = SQLAlchemy()
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tmp/test.db'
+db = SQLAlchemy(app)
+
+
+class UserType(enum.Enum):
+    ADMIN = 0
+    PATIENT = 1
+    DOCTOR = 2
+
+
+specializations = db.Table('specializations',
+                           db.Column('doctor_id', db.Integer, db.ForeignKey('users.id'), primary_key=True),
+                           db.Column('specialization_id', db.Integer, db.ForeignKey('specialization.id'),
+                                     primary_key=True)
+                           )
 
 
 class User(db.Model):
@@ -16,7 +33,10 @@ class User(db.Model):
     dob = db.Column(db.Date)
     phone_number = db.Column(db.String(20))
     created_at = db.Column(db.DateTime, server_default=func.current_timestamp())
-    user_type = db.Column(db.Integer)
+    user_type = db.Column(db.Enum(UserType))
+
+    specializations = db.relationship('Specialization', secondary=specializations, lazy='subquery',
+                                      backref=db.backref('doctors', lazy=True))
 
     def __init__(self, email, first_name, last_name, dob, phone_number, user_type):
         self.email = email
@@ -33,5 +53,50 @@ class User(db.Model):
         return check_password_hash(self.password, password)
 
 
+class Specialization(db.Model):
+    __tablename__ = 'specialization'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
 
 
+class Appointment(db.Model):
+    __tablename__ = 'appointments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    patient_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    doctor_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    patient = db.relationship('User', foreign_keys=[patient_id], backref='p_appointments')
+    doctor = db.relationship('User', foreign_keys=[doctor_id], backref='d_appointments')
+    description = db.Column(db.String())
+    date = db.Column(db.Date, nullable=False)
+    start = db.Column(db.Time, nullable=False)
+    end = db.Column(db.Time, nullable=False)
+    status = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, description, date, start, end, status=1):
+        self.description = description
+        self.date = date
+        self.start = start
+        self.end = end
+        self.status = status
+
+
+class Prescription(db.Model):
+    __tablename__ = 'prescriptions'
+
+    id = db.Column(db.Integer, primary_key=True)
+    patient_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    doctor_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    patient = db.relationship('User', foreign_keys=[patient_id], backref='p_prescriptions')
+    doctor = db.relationship('User', foreign_keys=[doctor_id], backref='d_prescriptions')
+    drug = db.Column(db.String(), nullable=False)
+    dosage = db.Column(db.String(), nullable=False)
+    date = db.Column(db.Date, nullable=False)
+    status = db.Column(db.Integer)
+
+    def __init__(self, drug, dosage, date, status=1):
+        self.drug = drug
+        self.date = date
+        self.dosage = dosage
+        self.status = status
