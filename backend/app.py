33567c4b05c3bc1af8eafbea57de0361ceb93128
jplulu <lujason899@gmail.com>
2020-11-26 13:56:04 -0500

    Added findDoctor, addAppointment, getAppointment, updateAppointmentStatus

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
 from backend import app
 from backend.routes.user_routes import user_routes
+from backend.routes.appointment_routes import appointment_routes
 from backend.db import db
 
 app.register_blueprint(user_routes)
+app.register_blueprint(appointment_routes)
 
 
 @app.before_first_request
